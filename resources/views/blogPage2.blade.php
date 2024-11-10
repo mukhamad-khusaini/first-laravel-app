2b6e7ca4563c5a94da8c5f28a9da0c6a4d98aea3
hasnaya <khusainimukhamad@gmail.com>
2024-11-10 23:36:49 +0700

    Add tailwind

--- resources/views/blogPage2.blade.php
+++ resources/views/blogPage2.blade.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Cara Membuat Uang</title>
 </head>
 <body>
-    <h3>Cara Membuat Uang</h3>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, possimus quidem soluta debitis adipisci reiciendis iure molestiae consequuntur porro nobis quos, consequatur repellat, aut accusantium? Sapiente ea consequuntur delectus rem.
-    Sapiente quidem nobis, reiciendis accusantium eveniet quam nesciunt distinctio, reprehenderit commodi dolore at necessitatibus recusandae, provident repellat placeat voluptatibus dolorum officia similique quae error soluta fugit? Minima facilis odit iste.
-    Cumque, reiciendis, eveniet enim error expedita fugit aspernatur, quas quod quos eius numquam iste vitae suscipit officia sed! Modi aut corrupti accusamus quod mollitia commodi similique, saepe reprehenderit quia officia?</p>
+    <div class="w-full h-[100vh] flex flex-col px-[20rem] align-middle justify-center">
+        <h3 class="font-bold text-blue-800 mb-2">Cara Membuat Uang</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, possimus quidem soluta debitis adipisci reiciendis iure molestiae consequuntur porro nobis quos, consequatur repellat, aut accusantium? Sapiente ea consequuntur delectus rem.
+        Sapiente quidem nobis, reiciendis accusantium eveniet quam nesciunt distinctio, reprehenderit commodi dolore at necessitatibus recusandae, provident repellat placeat voluptatibus dolorum officia similique quae error soluta fugit? Minima facilis odit iste.
+        Cumque, reiciendis, eveniet enim error expedita fugit aspernatur, quas quod quos eius numquam iste vitae suscipit officia sed! Modi aut corrupti accusamus quod mollitia commodi similique, saepe reprehenderit quia officia?</p>
+    </div>
+    <div><p>asda</p></div>
 </body>
 </html>
