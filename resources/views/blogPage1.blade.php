71fc55459717f96dfcd159a74e96a62f51a91b1e
hasnaya <khusainimukhamad@gmail.com>
2024-11-11 11:04:28 +0700

    Add alpineJs

--- resources/views/blogPage1.blade.php
+++ resources/views/blogPage1.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,20 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Cara Cepat Kaya</title>
 </head>
+
 <body>
     <div class="w-full h-[100vh] flex flex-col px-[20rem] align-middle justify-center">
         <h3 class="font-bold text-blue-800 mb-2">Cara Cepat Kaya</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, possimus quidem soluta debitis adipisci reiciendis iure molestiae consequuntur porro nobis quos, consequatur repellat, aut accusantium? Sapiente ea consequuntur delectus rem.
-        Sapiente quidem nobis, reiciendis accusantium eveniet quam nesciunt distinctio, reprehenderit commodi dolore at necessitatibus recusandae, provident repellat placeat voluptatibus dolorum officia similique quae error soluta fugit? Minima facilis odit iste.
-        Cumque, reiciendis, eveniet enim error expedita fugit aspernatur, quas quod quos eius numquam iste vitae suscipit officia sed! Modi aut corrupti accusamus quod mollitia commodi similique, saepe reprehenderit quia officia?</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, possimus quidem soluta debitis adipisci
+            reiciendis iure molestiae consequuntur porro nobis quos, consequatur repellat, aut accusantium? Sapiente ea
+            consequuntur delectus rem.
+            Sapiente quidem nobis, reiciendis accusantium eveniet quam nesciunt distinctio, reprehenderit commodi dolore
+            at necessitatibus recusandae, provident repellat placeat voluptatibus dolorum officia similique quae error
+            soluta fugit? Minima facilis odit iste.
+            Cumque, reiciendis, eveniet enim error expedita fugit aspernatur, quas quod quos eius numquam iste vitae
+            suscipit officia sed! Modi aut corrupti accusamus quod mollitia commodi similique, saepe reprehenderit quia
+            officia?</p>
     </div>
 </body>
+
 </html>
