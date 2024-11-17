c3c138d91e70f1ed1870dab2a7d10de44513a3cd
hasnaya <khusainimukhamad@gmail.com>
2024-11-17 20:12:50 +0700

    Create database migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
-use PhpParser\Node\Expr\Cast\Array_;
 
 class Post
 {
@@ -20,16 +19,7 @@ class Post
             'title' => "Cara Membuat Uang",
             "author" => "Mukhamad Khusaini",
             "tag" => "Strategy",
-            "content" => "
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum harum dicta assumenda dignissimos
-                    quae illum, cumque consequatur dolores labore architecto in consectetur, mollitia quo eum odit
-                    facilis ratione. Pariatur, sunt!
-                    Atque doloremque maiores mollitia ipsa vel aspernatur, neque facere quo eos eligendi inventore
-                    voluptatum, optio quos minus unde. Ratione sapiente amet nisi iste accusantium consequatur, esse
-                    deleniti ipsam quo temporibus?
-                    Molestiae asperiores animi nobis libero aut autem iure, hic at et saepe magni soluta nam vero natus
-                    nesciunt explicabo ex ipsam voluptatem repudiandae non ipsa itaque distinctio ratione a? Tenetur!
-            "
+            "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit consectetur maiores iure, ipsa et architecto dolore reprehenderit aliquid ducimus molestias vero eaque ad omnis voluptates aut, earum quasi consequatur ex?Tempora iusto, cumque deserunt voluptates reiciendis, saepe et illo necessitatibus, maxime quibusdam ratione est? Et, voluptas voluptatem? Excepturi eius aut eveniet, nesciunt quod quaerat doloribus enim quisquam placeat modi at? "
         ]
     ];
     public static function all(): array
