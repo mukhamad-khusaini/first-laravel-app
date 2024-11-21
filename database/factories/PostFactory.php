3d228977de32460dc05c5209533d6ffc900479fd
hasnaya <khusainimukhamad@gmail.com>
2024-11-21 21:51:00 +0700

    Removing dot from text generated by faker sentence

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,7 +18,7 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
-        $title = fake()->sentence(6);
+        $title = substr(fake()->sentence(6), 0, -1);
         $slug = Str::slug($title);
         return [
             'title' => $title,
